406fb64eaaac8b3d4878f84b17afad9c69e40a43
daseul21 <daseul_kim@icloud.com>
2016-03-23 20:34:33 +0900

    work

---
+++ src/com/j2/wk3/TurtleComparator.java
@@ -0,0 +1,24 @@
+package com.j2.wk3;
+
+import com.sd.turtle.WeightTurtle;
+
+public class TurtleComparator {
+    public static void main(String[] args) {
+
+        WeightTurtle wt30 = new WeightTurtle(30);
+        WeightTurtle wt10 = new WeightTurtle(10);
+        WeightTurtle wt20 = new WeightTurtle(20);
+        WeightTurtle[] wts={wt30, wt10, wt20};
+        Comparator turtleComp=new turtleComparator();
+        Sorter.sort(wts, turtleComp);
+
+        for(int i=0; i<wts.length; i++)
+            System.out.println("WeightTurtleArray["+i+"]="+wts[i].getWeight());
+    }
+}
+
+class turtleComparator implements Comparator {
+    public int compare(Object t1, Object t2) {
+        return ((WeightTurtle)t1).getWeight()-((WeightTurtle)t2).getWeight();
+    }
+}
