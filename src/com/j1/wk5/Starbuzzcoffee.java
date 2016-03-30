cae65e126c3a2c0dbf66f8a955c74120bff63d9a
daseul21 <daseul_kim@icloud.com>
2016-03-30 19:42:26 +0900

    coffee

---
+++ src/com/j1/wk5/Starbuzzcoffee.java
@@ -0,0 +1,81 @@
+package com.j1.wk5;
+
+
+abstract class Beverage{
+  String description =  "Unknown beverage";
+  public String getDescription(){
+    return description;
+  }
+  public abstract double cost();
+}
+
+
+class Espresso extends Beverage{
+  public Espresso(){
+    description="Espresso";
+  }
+  public double cost(){
+    return 1.99;
+  }
+}
+
+
+class DarkRoast extends Beverage{
+  public DarkRoast(){
+    description ="Dark Roast Coffee";
+  }
+  public double cost(){
+    return .99;
+  }
+}
+abstract class CondimentDecorator extends Beverage{
+  Beverage beverage;
+  public abstract String getDescription();
+}
+
+
+class Mocha extends CondimentDecorator{
+  Beverage beverage;
+  public Mocha(Beverage b){
+    beverage = b;
+  }
+  public String getDescription(){
+    return beverage.getDescription()+", Mocha";
+  }
+  public double cost(){
+    return beverage.cost()+.20;
+  }
+}
+
+
+
+
+class Whip extends CondimentDecorator{
+  Beverage beverage;
+  public Whip(Beverage b){
+    beverage=b;
+  }
+  public String getDescription(){
+    return beverage.getDescription()+",whip";
+  }
+  public double cost(){
+    return beverage.cost()+.10;
+  }
+}
+
+
+
+
+public class StarbuzzCoffee{
+  public static void main(String args[]){
+    Beverage b = new DarkRoast();
+    Beverage md = new Mocha(b);
+    Beverage wmd = new Whip(b);
+    
+    System.out.println(wmd.getDescription());
+    System.out.println(wmd.cost());
+  
+  }
+
+
+}
