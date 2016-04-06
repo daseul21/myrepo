ddf58c33cb2c7e4608753a1e0fcd680d9faed627
joowonsuk <ty921115@naver.com>
2016-04-06 18:03:36 +0900

    pizza

---
+++ src/com/j1/wk6/CheesePizza.java
@@ -0,0 +1,8 @@
+package com.j1.wk6;
+
+public class CheesePizza extends Pizza{
+  public CheesePizza(){
+    name ="Cheese Pizza";
+  }
+
+}
---
+++ src/com/j1/wk6/VeggiePizza.java
@@ -0,0 +1,8 @@
+package com.j1.wk6;
+
+public class VeggiePizza extends Pizza{
+  public VeggiePizza(){
+    name ="Veggie Pizza";
+  }
+
+}
